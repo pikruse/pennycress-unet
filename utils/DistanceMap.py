fixed distance weighting yeehaw!

utils/DistanceMap.py
@@ -6,12 +6,54 @@ import matplotlib.pyplot as plt
 
 from PIL import Image
 from skimage.io import imshow
+from scipy import ndimage
 from skimage.measure import label
 from scipy.ndimage import distance_transform_edt
 
 # define Distance Map function
 
-def distance_map(image, wc = None, wb = 10, bwidth = 5):
+def distance_map_bw(image, wb = 10, bwidth = 5):
+    """
+    function to create distance map from grayscale image
+
+    Parameters:
+        image (numpy array): input 1-channel boolean mask (MUST BE T-F)
+        wb (int): weight for border pixels
+        bwidth (int): border width parameter
+    
+    Returns:
+        distance weight map (numpy array of same size as input image)
+    """
+
+    # multiply image by inverse of binary erosion
+    image = image * ~ndimage.binary_erosion(image, iterations=1)
+
+    # calculate distances away from border
+    distance = distance_transform_edt(~image)
+
+    # set distance cutoff at bwidth max and invert
+    weights = bwidth - distance.clip(max = bwidth)
+
+    # normalize weights and multiply by wb param.
+    w = (weights / weights.max()) * wb
+
+    if wb < 1:
+        # invert the weights
+
+        # min/max normalize to 0-1
+        w = (w - w.min()) / (w.max() - w.min())
+
+        # invert the weights
+        w = 1 - w
+
+        # multiply by (1 - wb) and then add wb to get min wb and max 1 
+        w = (w * (1 - wb)) + (wb)  
+    else:
+        w = w.clip(min = 1)
+
+    return w
+
+def distance_map_rgb(image, wb = 10, bwidth = 5, wc = None):
 
     """
     (code modified from https://gist.github.com/rok/5f4314ed3c294521456c6afda36a3a50)
@@ -55,8 +97,20 @@ def distance_map(image, wc = None, wb = 10, bwidth = 5):
 
     # calculate weights (border param. * exp(-1/2 * (d1 + d2) / sigma) * no_labels
     w = wb * np.exp(-1/2*((d1 + d2) / bwidth)**2)
-    w += 1
-    
+
+    if wb < 1:
+        # invert the weights
+
+        # min/max normalize to 0-1
+        w = (w - w.min()) / (w.max() - w.min())
+
+        # invert the weights
+        w = 1 - w
+
+        # multiply by (1 - wb) and then add wb to get min wb and max 1 
+        w = (w * (1 - wb)) + (wb)  
+    else:
+        w = w.clip(min = 1)
 
     # if weight classes are provided
     if wc:

utils/TileGenerator.py
@@ -11,7 +11,7 @@ from scipy.ndimage import distance_transform_edt, binary_dilation
 from PIL import Image
 
 # import custom weight function
-import utils.DistanceMap as DistanceMap
+import utils.DistanceMapRGB as DistanceMapRGB
 
 
 
@@ -106,7 +106,7 @@ class TileGenerator(Dataset):
         # distance weighting 
         if self.distance_weights:
             weights_input = mask.astype(np.uint8)[:, :, 1:] * 255
-            weights = DistanceMap.distance_map(weights_input, wc = {'wing': 1, 'env': 1, 'seed': 1.2}, wb = .5, bwidth = 5)
+            weights = DistanceMapRGB.distance_map(weights_input, wc = {'wing': 1, 'env': 1, 'seed': 1.2}, wb = .5, bwidth = 5)
         
         #convert to torch tensor
         tile = torch.from_numpy(tile.transpose(2, 0, 1)) #convert to torch tensor and transpose to channels first